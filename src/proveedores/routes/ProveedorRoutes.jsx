0d491f535523d1be4889edc4e54e44b1fb971ad6
RicardoMolinaCoronel <ridumoli@espol.edu.ec>
2023-10-24 16:38:44 -0500

    Formateo

--- src/proveedores/routes/ProveedorRoutes.jsx
+++ src/proveedores/routes/ProveedorRoutes.jsx
@@ -1,36 +1,53 @@
-import {React} from 'react'
-import { Routes, Route } from 'react-router-native'
-import StyledText from '../../styles/StyledText'
-import { StatusBar } from 'expo-status-bar'
-import {View, StyleSheet} from 'react-native'
-import { Navigate, useNavigate, useLocation } from 'react-router-native'
+import { React } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-native";
+import StyledText from "../../styles/StyledText";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
 
-import HomeRoutes from './HomeRoutes'
-import AppProvBar from '../components/AppProvBar'
-import NavigationBar from '../components/NavigationBar'
-const ProveedorRoutes = ({closeButtonOffset, scaleValue, offsetValue, showMenu, setShowMenu}) => {
+import HomeRoutes from "./HomeRoutes";
+import AppProvBar from "../components/AppProvBar";
+import NavigationBar from "../components/NavigationBar";
+const ProveedorRoutes = ({
+  closeButtonOffset,
+  scaleValue,
+  offsetValue,
+  showMenu,
+  setShowMenu,
+}) => {
+  const location = useLocation();
 
-let location = useLocation();
-
-  return(
-        <Routes>
-        <Route path='/proveedor/home/*' element={
+  return (
+    <Routes>
+      <Route
+        path="/proveedor/home/*"
+        element={
           <>
-          <AppProvBar closeButtonOffset={closeButtonOffset} scaleValue={scaleValue} offsetValue={offsetValue} showMenu={showMenu} setShowMenu={setShowMenu}/>   
-          <StatusBar style="light" />  
-<NavigationBar/> 
-<HomeRoutes/>
-{
-location.pathname=="/proveedor/home" && <Navigate to="/proveedor/home/ofertas"/>
-}
-</>
-          }/>
-          <Route path='/proveedor/profile' element={<View>
-          <StyledText fontWeight="bold"> PERFIL</StyledText>
-          <StatusBar style="light" />         
-          </View>}
-          />
-        </Routes>
-    )
-}
-export default ProveedorRoutes
+            <AppProvBar
+              closeButtonOffset={closeButtonOffset}
+              scaleValue={scaleValue}
+              offsetValue={offsetValue}
+              showMenu={showMenu}
+              setShowMenu={setShowMenu}
+            />
+            <StatusBar style="light" />
+            <NavigationBar />
+            <HomeRoutes />
+            {location.pathname == "/proveedor/home" && (
+              <Navigate to="/proveedor/home/ofertas" />
+            )}
+          </>
+        }
+      />
+      <Route
+        path="/proveedor/profile"
+        element={
+          <View>
+            <StyledText fontWeight="bold"> PERFIL</StyledText>
+            <StatusBar style="light" />
+          </View>
+        }
+      />
+    </Routes>
+  );
+};
+export default ProveedorRoutes;
