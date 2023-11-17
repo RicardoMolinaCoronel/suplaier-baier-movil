1842e9e02e817df688d658e3e2dc3105c554def6
sergio basurto cedeño <sbasurto@espol.edu.ec>
2023-11-17 13:50:44 -0500

    Búsqueda probado y corregido

--- src/proveedores/pages/SearchProveedorPage.jsx
+++ src/proveedores/pages/SearchProveedorPage.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList, ScrollView } from "react-native";
 import StyledText from "../../styles/StyledText";
 import { StatusBar } from "expo-status-bar";
 import Search_Input from '../components/Search_Input';
@@ -8,35 +8,24 @@ import theme from "../../theme";
 import Cargar_Categorias from '../components/Cargar_Categorias';
 import { apiUrl } from '../../../apiUrl';
 import {useEffect, useState} from 'react'
+import OfertaItem from "../components/OfertaItem";
 const SearchProveedorPage = () => {
   const { ofertasBusqueda, getOfertasTodos } = Search_Logic();
   const [showEmptyArray, setShowEmptyArray] = useState(false);
 
   const handleSearch = async (searchText) => {
-    const resp = await fetch(`${apiUrl}/ofertaByProducto?q=${searchText}`);
-    const data = await resp.json();
-    const { rows: ofertas } = data || {};
+ // Actualiza los resultados en Search_Logic usando la función getOfertasTodos
+  getOfertasTodos(searchText);
 
-    if (ofertas) {
-      // Filtra las ofertas que cumplan con ciertas condiciones
-      const filteredOfertas = ofertas.filter((oferta) => oferta.IdEstadosOferta === 1);
-
-      // Actualiza los resultados en Search_Logic
-      getOfertasTodos(filteredOfertas);
-    }
-    if (ofertas.length === 0) {
-      setShowEmptyArray(true);
-    } else {
-      setShowEmptyArray(false);
-      // Filtra y actualiza los resultados
-    }
-  };
+ // Verifica si la búsqueda está vacía o tiene resultados
+  setShowEmptyArray(ofertasBusqueda.length === 0);
+    };
   useEffect(() => {
     // Cuando el componente se monta, carga las ofertas iniciales
     getOfertasTodos('');
   }, []);
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
     <View style={styles.busquedaContainer}>
       <View style={styles.topContainer}>
         <Icon name="keyboard-right-arrow-button" width={20} height={20} />
@@ -48,24 +37,29 @@ const SearchProveedorPage = () => {
           Búsqueda
         </StyledText>
       </View>
-      <Search_Input onSearch={handleSearch} />
+      <Search_Input onSearch={(text) => handleSearch(text)} />
       <Cargar_Categorias></Cargar_Categorias>
       {/* Muestra los resultados de la búsqueda */}
-    {ofertasBusqueda.map((result) => (
-        <Text>{result.idOferta}{result.Oferta}</Text>
-      ))} 
       {showEmptyArray && (
         
           <Text style={styles.textNothing} >
             No hay productos con ese nombre
           </Text>
         )}
+           {
+         <FlatList
+         style={styles.flatListContainer}
+         data={ofertasBusqueda}
+         renderItem={({ item: oferta }) => <OfertaItem {...oferta} />}
+       />
+      } 
+       
     </View>
       
     <StatusBar style="light" />
     <View style={styles.spaceBorder} />
 
-  </View>
+  </ScrollView>
   );
 };
 
@@ -96,7 +90,16 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginTop: 45,
     color: 'purple',
-  }
+  },
+  flatListContainer: {},
+  vacioContainer: {
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  iconContainer: {
+    marginTop: 15,
+  },
 });
 
 export default SearchProveedorPage;
